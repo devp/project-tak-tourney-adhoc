837c8dbef0617635dc5406b1692d9b7cb009c229
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-11 03:36:57 -0500

    minimal cli driver

---
+++ src/cli-driver.ts
@@ -0,0 +1,8 @@
+import { analyzeTournamentProgress } from "./tournament-analyzer.ts";
+
+function main() {
+  console.log('Hi');
+  console.log('analyzeTournamentProgress', !!analyzeTournamentProgress);
+}
+
+main();
