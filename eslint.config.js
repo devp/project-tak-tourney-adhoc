c086a0acb7c770a2dec21a03ca670d9bf2350b33
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-14 11:48:46 -0500

    tools: ignore generated guard files for lint and format

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,9 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,ts}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { ignores: ["**/*.guard.ts"] },
 ];
