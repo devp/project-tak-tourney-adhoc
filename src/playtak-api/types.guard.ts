99f8902ffed95c618aec6cb10682bd6c7adf5827
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-14 09:34:27 -0500

    type: generated type guards

---
+++ src/playtak-api/types.guard.ts
@@ -0,0 +1,67 @@
+/*
+ * Generated type guards for "types.ts".
+ * WARNING: Do not manually change this file.
+ */
+import { GameResultType, GameResult, GameListResponse } from "./types";
+
+export function isGameResultType(obj: unknown): obj is GameResultType {
+    const typedObj = obj as GameResultType
+    return (
+        (typedObj === "R-0" ||
+            typedObj === "0-R" ||
+            typedObj === "F-0" ||
+            typedObj === "0-F" ||
+            typedObj === "1/2-1/2" ||
+            typedObj === "1-0" ||
+            typedObj === "0-1")
+    )
+}
+
+export function isGameResult(obj: unknown): obj is GameResult {
+    const typedObj = obj as GameResult
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typeof typedObj["id"] === "number" &&
+        typeof typedObj["date"] === "number" &&
+        typeof typedObj["size"] === "number" &&
+        typeof typedObj["player_white"] === "string" &&
+        typeof typedObj["player_black"] === "string" &&
+        typeof typedObj["notation"] === "string" &&
+        isGameResultType(typedObj["result"]) as boolean &&
+        typeof typedObj["timertime"] === "number" &&
+        typeof typedObj["timerinc"] === "number" &&
+        typeof typedObj["rating_white"] === "number" &&
+        typeof typedObj["rating_black"] === "number" &&
+        (typedObj["unrated"] === 0 ||
+            typedObj["unrated"] === 1) &&
+        (typedObj["tournament"] === 0 ||
+            typedObj["tournament"] === 1 ||
+            typedObj["tournament"] === 2) &&
+        typeof typedObj["komi"] === "number" &&
+        typeof typedObj["pieces"] === "number" &&
+        typeof typedObj["capstones"] === "number" &&
+        typeof typedObj["rating_change_white"] === "number" &&
+        typeof typedObj["rating_change_black"] === "number" &&
+        typeof typedObj["extra_time_amount"] === "number" &&
+        typeof typedObj["extra_time_trigger"] === "number"
+    )
+}
+
+export function isGameListResponse(obj: unknown): obj is GameListResponse {
+    const typedObj = obj as GameListResponse
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["items"]) &&
+        typedObj["items"].every((e: any) =>
+            isGameResult(e) as boolean
+        ) &&
+        typeof typedObj["total"] === "number" &&
+        typeof typedObj["page"] === "number" &&
+        typeof typedObj["perPage"] === "number" &&
+        typeof typedObj["totalPages"] === "number"
+    )
+}
---
+++ src/types.guard.ts
@@ -0,0 +1,121 @@
+/*
+ * Generated type guards for "types.ts".
+ * WARNING: Do not manually change this file.
+ */
+import { TournamentType, TournamentPlayer, WinnerMethod, TournamentGroup, TournamentStatusBase, GroupTournamentStatus, KnockoutTournamentStatus, TournamentStatus, TournamentInfo } from "./types";
+
+export function isTournamentType(obj: unknown): obj is TournamentType {
+    const typedObj = obj as TournamentType
+    return (
+        (typedObj === "groupStage" ||
+            typedObj === "knockoutStage")
+    )
+}
+
+export function isTournamentPlayer(obj: unknown): obj is TournamentPlayer {
+    const typedObj = obj as TournamentPlayer
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typeof typedObj["username"] === "string" &&
+        typeof typedObj["group"] === "string" &&
+        typeof typedObj["score"] === "number" &&
+        typeof typedObj["games_played"] === "number"
+    )
+}
+
+export function isWinnerMethod(obj: unknown): obj is WinnerMethod {
+    const typedObj = obj as WinnerMethod
+    return (
+        (typedObj === "score" ||
+            typedObj === "head-to-head" ||
+            typedObj === "sonneborn-berger" ||
+            typedObj === "blitz" ||
+            typedObj === "all-methods-exhausted")
+    )
+}
+
+export function isTournamentGroup(obj: unknown): obj is TournamentGroup {
+    const typedObj = obj as TournamentGroup
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typeof typedObj["name"] === "string" &&
+        (isTournamentPlayer(typedObj["winner"]) as boolean ||
+            Array.isArray(typedObj["winner"]) &&
+            typedObj["winner"].every((e: any) =>
+                isTournamentPlayer(e) as boolean
+            )) &&
+        isWinnerMethod(typedObj["winner_method"]) as boolean
+    )
+}
+
+export function isTournamentStatusBase(obj: unknown): obj is TournamentStatusBase {
+    const typedObj = obj as TournamentStatusBase
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        Array.isArray(typedObj["players"]) &&
+        typedObj["players"].every((e: any) =>
+            isTournamentPlayer(e) as boolean
+        )
+    )
+}
+
+export function isGroupTournamentStatus(obj: unknown): obj is GroupTournamentStatus {
+    const typedObj = obj as GroupTournamentStatus
+    return (
+        isTournamentStatusBase(typedObj) as boolean &&
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typedObj["tournamentType"] === "groupStage" &&
+        Array.isArray(typedObj["groups"]) &&
+        typedObj["groups"].every((e: any) =>
+            isTournamentGroup(e) as boolean
+        )
+    )
+}
+
+export function isKnockoutTournamentStatus(obj: unknown): obj is KnockoutTournamentStatus {
+    const typedObj = obj as KnockoutTournamentStatus
+    return (
+        isTournamentStatusBase(typedObj) as boolean &&
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typedObj["tournamentType"] === "knockoutStage"
+    )
+}
+
+export function isTournamentStatus(obj: unknown): obj is TournamentStatus {
+    const typedObj = obj as TournamentStatus
+    return (
+        (isGroupTournamentStatus(typedObj) as boolean ||
+            isKnockoutTournamentStatus(typedObj) as boolean)
+    )
+}
+
+export function isTournamentInfo(obj: unknown): obj is TournamentInfo {
+    const typedObj = obj as TournamentInfo
+    return (
+        (typedObj !== null &&
+            typeof typedObj === "object" ||
+            typeof typedObj === "function") &&
+        typeof typedObj["name"] === "string" &&
+        isTournamentType(typedObj["tournamentType"]) as boolean &&
+        (typedObj["dateRange"] !== null &&
+            typeof typedObj["dateRange"] === "object" ||
+            typeof typedObj["dateRange"] === "function") &&
+        typedObj["dateRange"]["start"] instanceof Date &&
+        typedObj["dateRange"]["end"] instanceof Date &&
+        Array.isArray(typedObj["players"]) &&
+        typedObj["players"].every((e: any) =>
+            isTournamentPlayer(e) as boolean
+        ) &&
+        isTournamentStatus(typedObj["status"]) as boolean
+    )
+}
