f75a6087aba300767dabdc0985b58f2874718a9a
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-22 21:21:42 -0500

    fix: export type guards also

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ export { analyzeTournamentProgress } from "./tournament-analyzer.ts";
 
 // Re-export types in namespaces
 import * as TournamentStatusTypes from "./types.ts";
+import * as TournamentStatusTypeGuards from "./types.guard.ts";
 import * as PlaytakApiTypes from "./playtak-api/types.ts";
-export { TournamentStatusTypes, PlaytakApiTypes };
+import * as PlaytakApiTypeGuards from "./playtak-api/types.guard.ts";
+export { TournamentStatusTypes, PlaytakApiTypes, TournamentStatusTypeGuards, PlaytakApiTypeGuards };
 
 // Re-export constants in namespace
 import * as GameResultConstants from "./constants.ts";
