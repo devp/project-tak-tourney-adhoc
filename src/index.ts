73705aaaa44c3348667a3d91a12797326e66bd76
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-23 08:33:39 -0500

    deps: adjust types export

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ export { analyzeTournamentProgress } from "./tournament-analyzer.ts";
 
 // Re-export types in namespaces
 import * as TournamentStatusTypes from "./types.ts";
-import * as TournamentStatusTypeGuards from "./types.guard.ts";
-import * as PlaytakApiTypes from "./playtak-api/types.ts";
 import * as PlaytakApiTypeGuards from "./playtak-api/types.guard.ts";
-export { TournamentStatusTypes, PlaytakApiTypes, TournamentStatusTypeGuards, PlaytakApiTypeGuards };
+export { TournamentStatusTypeGuards, PlaytakApiTypeGuards };
+import type * as TournamentStatusTypeGuards from "./types.guard.ts";
+import type * as PlaytakApiTypes from "./playtak-api/types.ts";
+export type { TournamentStatusTypes, PlaytakApiTypes };
 
 // Re-export constants in namespace
 import * as GameResultConstants from "./constants.ts";
